704e6c01e7e8b66c154907af04c00db3530b228a
Mohamad <matse.85@hotmail.com>
2019-05-28 09:27:03 +0200

    print with utf-8 coding

--- py_SA_2/makro_expandierung.py
+++ py_SA_2/makro_expandierung.py
@@ -4,13 +4,16 @@ import analyse
 import makro
 
 
-datei = ''
 # Aufrufen: python3 codeDatei.py datei.txt
 # sys.argv = ['codeDatei.py', 'datei.txt']
-if(len(sys.argv) == 2):
-    datei = sys.argv[1] # datei = 'datei.txt'
-else:
-    print('Bitte nur zwei Argumente nach python3\nprobieren Sie nochmal:') # der Interpreter soll hier abbrechen.
+# datei = ''
+# if(len(sys.argv) == 2):
+#     datei = sys.argv[1] # datei = 'datei.txt'
+# else:
+#     print('Bitte nur zwei Argumente nach python3\nprobieren Sie nochmal:') # der Interpreter soll hier abbrechen.
+
+# oder:
+datei = sys.argv[1]
 
 f = open(datei, "r")
 s = f.read()
--- py_SA_2/test.py
+++ py_SA_2/test.py
@@ -1,12 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 s = 'hi hello, world.'
 
 b = list(s)
-print('b=',b)
+# print('b=',b)
 
-# to remove , and . 
+# to remove , and . (nutzlich mit Worter, die mit /def/ anfangen, damit g nicht , oder . sein)
 a = s.split(' ')
-print('a=',a)
-for i in a:
-    if (i.endswith(',')) or (i.endswith('.')):
-        a[a.index(i)] = i[:len(i)-1]
-print('a=',a)
+# print('a=',a)
+
+c = ' '.join(a)
+# print(c)
+
+# for i in a:
+#     if (i.endswith(',')) or (i.endswith('.')):
+#         a[a.index(i)] = i[:len(i)-1]
+# print('a=',a)
+
+
+# Analyse:
+s = 'Noch wird \abc nicht veraendert. Aber jetzt kommt eine Makrodefinition \def\abc?abcA\?B\?Cabc?, die dann hier \abc zu einer Textersetzung f�hrt.'
+# ss = s.encode(encoding='UTF-8', errors='strict')
+# ss = s.decode('utf8')
+
+print(s)
+
+# a = s.split(' ')
+# for i in a:
+# #     if(i.startswith('\def\\')):
+# #     if(i[1:4]=='def'):
+#     print(i)
+print('\def\\') 
+        
